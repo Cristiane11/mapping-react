11e8eb7c88441445c9f5a3541e2eef8bcbbd5178
Cristiane11 <bjwmcd@yahoo.com>
2020-05-01 05:43:18 -0400

    update

---
+++ mapping-react/src/component/CatCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function CatCard(){
+    return(
+        <div className="contact-card">
+                <img src="http://placekitten.com/400/300"/>
+                <h3>Destroyer</h3>
+                <p>Phone: (212) 555-3456</p>
+                <p>Email: ofworlds@yahoo.com</p>
+        </div>
+    )
+}
+export default CatCard;
