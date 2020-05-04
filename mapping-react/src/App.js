2c9cc8e7eccf58867f09ece80d683f63daf260e2
Cristiane11 <bjwmcd@yahoo.com>
2020-05-03 21:44:08 -0400

    update

--- mapping-react/src/App.js
+++ mapping-react/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CatCard from './component/CatCard';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +13,7 @@ function App() {
       <CatCard name="Mr. Whiskerson" imgUrl="http://placekitten.com/300/200" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
     </div>
   );
+}
 }
 
 export default App;
