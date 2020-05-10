b3b54f429d22520f654902d6177339c4a61c870d
Cristiane11 <bjwmcd@yahoo.com>
2020-05-10 03:23:53 -0400

    update

--- mapping-react/src/App.js
+++ mapping-react/src/App.js
@@ -3,14 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import CatCard from './component/CatCard';
 
+
 class App extends Component {
+  state={
+    count:1
+  }
+  
   render(){
+   
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <CatCard name="Mr. Whiskerson" imgUrl="http://placekitten.com/300/200" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
+     
+  <p>Count: {this.state.count}</p>
     </div>
   );
 }
---
+++ mapping-react/src/State1.js
@@ -0,0 +1,18 @@
+import React, {Component} from '.react';
+
+
+class State1 extends Component{
+        constructor(){
+        super()
+        this.state ={
+        count:0 
+
+        }
+    }
+    render(){
+        return(
+        <div>{state.count}</div>
+        );
+    }
+}
+export default State1;
