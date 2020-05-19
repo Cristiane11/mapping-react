82e188117a2a4dcc55cda318ff8eeb217eb4a0f2
Cristiane11 <bjwmcd@yahoo.com>
2020-05-19 06:25:37 -0400

    update

--- mapping-react/src/component/Square.js
+++ mapping-react/src/component/Square.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-
+const tiles =['','0','X'];
 export default class Square extends Component {
+  state ={
+    currentBoard:0
+  }
+  handleClick(){
+    this.setState(prevState=>{
+      return{
+        currentBoard:(prevState.currentBoard + 4)%3
+      }
+    })
+  }
   render(){
     return <div className='squareComponent'
-              onClick={this.props.handleClick}>{this.props.value}
+              onClick={this.handleClick.bind(this)}>{this.state.currentBoard}
             </div>
   }
 }
