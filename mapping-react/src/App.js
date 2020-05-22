31a4fdc8ea5f49bc6fb03648455aaee7f2ab534b
Cristiane11 <bjwmcd@yahoo.com>
2020-05-22 05:19:37 -0400

    update

--- mapping-react/src/App.js
+++ mapping-react/src/App.js
@@ -16,7 +16,18 @@ class App extends Component {
   }
   
   playerWon(){
-    const board = this.state.board
+    const board = this.state.board;
+    const winningConditions =[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    
   }
 
    updateBoard(i){
